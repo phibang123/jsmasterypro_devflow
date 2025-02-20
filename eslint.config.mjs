f27d90793eb23e31e324f1932205520dc1ef4181
Bang <bangtoken123@gmail.com>
2025-02-20 13:54:32 +0700

    update package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,68 +1,77 @@
+import _import from "eslint-plugin-import";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import { fixupPluginRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [
-  {
+export default [{
     ignores: ["components/ui/**/*"],
-  },
-  ...compat.extends(
+}, ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
     "standard",
     "plugin:tailwindcss/recommended",
-    "prettier"
-  ),
-  {
-    rules: {
-      "import/order": [
-        "error",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling"],
-            "index",
-            "object",
-          ],
-
-          "newlines-between": "always",
-
-          pathGroups: [
-            {
-              pattern: "@app/**",
-              group: "external",
-              position: "after",
-            },
-          ],
-
-          pathGroupsExcludedImportTypes: ["builtin"],
-
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-        },
-      ],
+    "prettier",
+), {
+    plugins: {
+        import: fixupPluginRules(_import),
+        "@typescript-eslint": typescriptEslint,
     },
-  },
-  {
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        "import/order": ["error", {
+            groups: [
+                "builtin",
+                "external",
+                "internal",
+                ["parent", "sibling"],
+                "index",
+                "object",
+            ],
+
+            "newlines-between": "always",
+
+            pathGroups: [{
+                pattern: "@app/**",
+                group: "external",
+                position: "after",
+            }],
+
+            pathGroupsExcludedImportTypes: ["builtin"],
+
+            alphabetize: {
+                order: "asc",
+                caseInsensitive: true,
+            },
+        }],
+
+        semi: ["error", "always"],
+    },
+}, {
     files: ["**/*.ts", "**/*.tsx"],
 
     rules: {
-      "no-undef": "off",
+        "no-undef": "off",
     },
-  },
-];
+}];
