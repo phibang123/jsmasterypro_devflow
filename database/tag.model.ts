207cae32bc9b9524041a481f8771406164ee5db6
Bang Tran <kona.tran@hitachids.com>
2025-04-01 15:45:50 +0700

    [Create] Tag model

---
+++ database/tag.model.ts
@@ -0,0 +1,18 @@
+import { model, models, Schema } from "mongoose";
+
+interface ITag {
+  name: string;
+  questions: number;
+}
+
+const TagSchema = new Schema<ITag>(
+  {
+    name: { type: Schema.Types.String, required: true },
+    questions: { type: Schema.Types.Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Tag = models?.Tag && model<ITag>("Tag", TagSchema);
+
+export default Tag;
