9807bef012baf4f9459c646b29634dcc25e98e57
Bang Tran <kona.tran@hitachids.com>
2025-04-01 10:15:54 +0700

    [Update] Account model

--- database/account.model.ts
+++ database/account.model.ts
@@ -9,7 +9,7 @@ export interface IAccount {
   providerAccountId: string;
 }
 
-const AccountSchema = new Schema(
+const AccountSchema = new Schema<IAccount>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
