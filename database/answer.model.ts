0c22fd062526f85821dd5212c286a674a1e61506
Bang Tran <kona.tran@hitachids.com>
2025-04-01 10:51:12 +0700

    [Create] Answer model

---
+++ database/answer.model.ts
@@ -0,0 +1,24 @@
+import { model, models, Schema, Types } from "mongoose";
+
+interface IAnswer {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+  content: string;
+  upVotes?: number;
+  downVotes?: number;
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    content: { type: String, required: true },
+    upVotes: { type: Number, default: 0 },
+    downVotes: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Answer = models.Answer || model<IAnswer>("Answer", AnswerSchema);
+
+export default Answer;
