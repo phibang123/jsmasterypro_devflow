ba57bcb523ed3001ea8dbcfc8a4d0e94e1945889
Bang Tran <kona.tran@hitachids.com>
2025-04-07 14:52:54 +0700

    [Implement] Account Provider API

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http.errors";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/response";
+
+// POST /api/users/email
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ data: account, success: true }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
