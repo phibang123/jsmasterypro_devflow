9d66e945a4adbf0b695d4be5418eeace08b63063
Bang Tran <kona.tran@hitachids.com>
2025-04-08 00:42:56 +0700

    [Implement] API constructor

---
+++ lib/api.ts
@@ -0,0 +1,54 @@
+import { IAccount } from "@/database/account.model";
+import { IUser } from "@/database/user.model";
+import { UserModelIF } from "@/types/model";
+
+import { axiosInstance } from "./handlers/axiosInstance";
+
+export const constructorApi = {
+  users: {
+    getAll: () => axiosInstance<IUser[]>("/users"),
+    getById: (id: string) => axiosInstance<IUser>(`/users/${id}`),
+    getEmail: (email: string) =>
+      axiosInstance<UserModelIF>(`/users/email`, {
+        method: "POST",
+        data: { email },
+      }),
+    create: (userData: Partial<IUser>) =>
+      axiosInstance<IUser>("users", {
+        method: "POST",
+        data: userData,
+      }),
+    update: (id: string, userData: Partial<IUser>) =>
+      axiosInstance<IUser>(`users/${id}`, {
+        method: "PUT",
+        data: userData,
+      }),
+    delete: (id: string) =>
+      axiosInstance<IUser>(`users/${id}`, {
+        method: "DELETE",
+      }),
+  },
+  accounts: {
+    getAll: () => axiosInstance<IAccount[]>("/accounts"),
+    getById: (id: string) => axiosInstance<IAccount>(`/accounts/${id}`),
+    getProvider: (providerAccountId: string) =>
+      axiosInstance<UserModelIF>(`/accounts/provider`, {
+        method: "POST",
+        data: { providerAccountId },
+      }),
+    create: (accountData: Partial<IAccount>) =>
+      axiosInstance<IAccount>("accounts", {
+        method: "POST",
+        data: accountData,
+      }),
+    update: (id: string, accountData: Partial<IAccount>) =>
+      axiosInstance<IAccount>(`accounts/${id}`, {
+        method: "PUT",
+        data: accountData,
+      }),
+    delete: (id: string) =>
+      axiosInstance<IAccount>(`accounts/${id}`, {
+        method: "DELETE",
+      }),
+  },
+};
