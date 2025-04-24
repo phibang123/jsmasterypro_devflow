43d8fbae13c10412f23b714978495c9a9b32a407
Bang Tran <phibang7899@gmail.com>
2025-04-24 17:12:50 +0700

    [Watch Log] Deploy v6

--- app/api/accounts/provider/route.ts
+++ app/api/accounts/provider/route.ts
@@ -3,23 +3,14 @@ import { NextRequest } from "next/server";
 import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error.handler";
 import handleSuccess from "@/lib/handlers/success.handler";
-import { NotFoundError, ValidationError } from "@/lib/http.errors";
+import { NotFoundError } from "@/lib/http.errors";
 import dbConnect from "@/lib/mongoose";
-import { AccountSchemaAPI } from "@/lib/validations";
 
 // POST /api/accounts/provider
 export async function POST(request: NextRequest) {
   try {
     const { providerAccountId } = await request.json();
     await dbConnect();
-
-    const validatedData = AccountSchemaAPI.partial().safeParse({
-      providerAccountId,
-    });
-
-    if (!validatedData.success) {
-      throw new ValidationError(validatedData.error.flatten().fieldErrors);
-    }
 
     const account = await Account.findOne({ providerAccountId });
     if (!account) throw new NotFoundError("Account");
