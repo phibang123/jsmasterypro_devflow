0b675cca2a8c88c71469902e6f977fc24f4b1861
Bang Tran <phibang7899@gmail.com>
2025-05-07 15:06:10 +0700

    [Update] package

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,32 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["pino", "pino-pretty"],
+  serverExternalPackages: ['pino', 'pino-pretty'],
   reactStrictMode: true,
   trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "static.vecteezy.com",
-        port: "",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'static.vecteezy.com',
+        port: '',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '**',
       },
     ],
-    domains: [
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
-      "static.vecteezy.com",
-    ],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'static.vecteezy.com'],
   },
 };
 
